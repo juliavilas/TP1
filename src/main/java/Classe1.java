337ab88dc672050c70af00a97b265387d84a7cca
Nelson Rogers <nelsonlrrogers@gmail.com>
2020-11-10 10:43:39 +0100

    Retour à l'étape d'avant

--- src/main/java/Classe1.java
+++ src/main/java/Classe1.java
@@ -11,7 +11,6 @@
 public class Classe1 {
     public static void main(String[] args) {
         System.out.println("Salut c'est Nelson");
-        System.out.println("Salut Nelson");
     }
 }
 // Blablabla
